a301e2e8d483d1aa201508b3085231bcdbf577c5
fnobi <shin@fnobi.com>
2013-01-12 16:35:20 +0900

    first commit.

---
+++ grunt.js
@@ -0,0 +1,18 @@
+module.exports = function (grunt) {
+	grunt.loadNpmTasks('grunt-jade');
+	grunt.initConfig({
+		jade: {
+			html: {
+				src: ['_templates/index.jade'],
+				dest: './',
+				options: {
+					client: false
+				}
+			}
+		},
+		watch: {
+			files: ['_templates/*.jade'],
+			tasks: 'jade'
+		}
+	});
+};
---
+++ javascripts/toc.js
@@ -0,0 +1,96 @@
+$(function () {
+	var TOC = function (selector) {
+		this.sections = [];
+	};
+
+	TOC.prototype.render = function () {
+		var $ul = this.$ul || $('<ul />');
+		$ul.empty();
+
+		this.sections.forEach(function (section) {
+			$ul.append(section.renderItem());
+		});
+
+		this.$ul = $ul;
+		return $ul[0];
+	};
+
+	TOC.prototype.addSection = function ($section) {
+		this.sections.push(new TOCSection(
+			$section,
+			this.sections.length + 1
+		));
+	};
+
+	var TOCSection = function ($section, index) {
+		this.$section = $section;
+		this.index = index;
+		this.initHeader();
+		this.initSubSections();
+	};
+
+	TOCSection.prototype.initHeader = function () {
+		var $section = this.$section;
+		var $header = $('h1', $section).first();
+		var index = this.index;
+
+		$header.attr('id', 'section' + index);
+		$header.prepend(index + ' ');
+
+		this.$header = $header;
+		return $header[0];
+	};
+
+	TOCSection.prototype.initSubSections = function () {
+		var self = this;
+		var $section = this.$section;
+		var indexpre = self.index + '-';
+		var subSections = [];
+
+		if ($('section', $section)[0]) {
+			$('section', $section).each(function () {
+				subSections.push(new TOCSection(
+					$(this),
+					indexpre + (subSections.length + 1)
+				));
+			});
+		}
+
+		this.subSections = subSections;
+		return subSections;
+	};
+
+	TOCSection.prototype.renderItem = function () {
+		if (this.$item) {
+			return this.$item[0];
+		}
+
+		var $header = this.$header;
+		var $item = $('<li />').append(
+			$('<a />')
+				.html($header.html())
+				.attr('href', '#' + $header.attr('id'))
+		);
+
+		var subSections = this.subSections;
+
+		if (subSections.length) {
+			var $list = $('<ul />');
+			subSections.forEach(function (subSection) {
+				$list.append(subSection.renderItem());
+			});
+			$item.append($list);
+		}
+
+		this.$item = $item;
+		return $item[0];
+	};
+
+	var toc = new TOC();
+
+	$('article > section').each(function () {
+		toc.addSection($(this));
+	});
+
+	$('#toc').append(toc.render());
+});
